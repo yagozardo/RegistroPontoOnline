e036d2983e0076e6c8acb38e92430bbf5e0f2baa
Robson Cardozo <robson.cardozo.junior@gmail.com>
2017-08-22 13:21:50 -0300

    Correção na entidade Adress

--- src/main/java/com/br/pontu/entity/Adress.java
+++ src/main/java/com/br/pontu/entity/Adress.java
@@ -15,115 +15,121 @@ import javax.validation.constraints.NotNull;
 import org.hibernate.validator.constraints.NotEmpty;
 
 /**
- * Classe utilitária que é para armazenamento tanto 
- * de um funcionário, quanto de uma empresa cadastrada
- * quanto um possível cliente e fornecedor pensando
- * em um sistema mais robusto.
- * 
+ * Classe utilitária que é para armazenamento tanto de um funcionário, quanto de
+ * uma empresa cadastrada quanto um possível cliente e fornecedor pensando em um
+ * sistema mais robusto.
+ *
  * @author Alves
  */
-
 @Entity
 @Table(name = "endereco_usuario")
 public class Adress implements Serializable {
-    
-	private static final long serialVersionUID = 1L;
-	
-	
-	/**
-	 *	Atributos 
+
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * Atributos
      */
-	private Long id;
-    private Long numero;
-    private String logradouro;
-    private String complemento;
-    private String cidade;
-    private String uf;
-    private String cep;
-    private User usuario;
-    
-    
-    
-    
-	//Getters and Setters
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    public Long getId() {
-		return id;
-	}
-	public void setId(Long id) {
-		this.id = id;
-	}
-	
-	@NotNull
-	@Column(nullable = false)
-	public Long getNumero() {
-		return numero;
-	}
-	public void setNumero(Long numero) {
-		this.numero = numero;
-	}
+    private Long id;
 
-	@NotNull
+    @NotNull
+    @Column(nullable = false)
+    private Long numero;
+
+    @NotNull
     @NotEmpty
     @Column(nullable = false, length = 60)
-	public String getLogradouro() {
-		return logradouro;
-	}
-	public void setLogradouro(String logradouro) {
-		this.logradouro = logradouro;
-	}
-	
-	@NotNull
+    private String logradouro;
+
+    @NotNull
     @NotEmpty
     @Column(nullable = false, length = 80)
-	public String getComplemento() {
-		return complemento;
-	}
-	public void setComplemento(String complemento) {
-		this.complemento = complemento;
-	}
-	
-	@NotNull
+    private String complemento;
+
+    @NotNull
     @NotEmpty
     @Column(nullable = false, length = 30)
-	public String getCidade() {
-		return cidade;
-	}
-	public void setCidade(String cidade) {
-		this.cidade = cidade;
-	}
-	
-	@NotNull
+    private String cidade;
+
+    @NotNull
     @NotEmpty
     @Column(nullable = false, length = 2)
-	public String getUf() {
-		return uf;
-	}
-	public void setUf(String uf) {
-		this.uf = uf;
-	}
-	
-	@NotNull
+    private String uf;
+
+    @NotNull
     @NotEmpty
     @Column(nullable = false, length = 8)
-	public String getCep() {
-		return cep;
-	}
-	public void setCep(String cep) {
-		this.cep = cep;
-	}
-	
-	@OneToOne
-	@JoinColumn(name = "usuario_id", nullable = false)
-	public User getUsuario() {
-		return usuario;
-	}
-	public void setUsuario(User usuario) {
-		this.usuario = usuario;
-	}
-    
-    
-    
-    
+    private String cep;
+
+    @OneToOne
+    @JoinColumn(name = "usuario_id", nullable = false)
+    private User usuario;
+
+    //Getters and Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getNumero() {
+        return numero;
+    }
+
+    public void setNumero(Long numero) {
+        this.numero = numero;
+    }
+
+    public String getLogradouro() {
+        return logradouro;
+    }
+
+    public void setLogradouro(String logradouro) {
+        this.logradouro = logradouro;
+    }
+
+    public String getComplemento() {
+        return complemento;
+    }
+
+    public void setComplemento(String complemento) {
+        this.complemento = complemento;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getUf() {
+        return uf;
+    }
+
+    public void setUf(String uf) {
+        this.uf = uf;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public User getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(User usuario) {
+        this.usuario = usuario;
+    }
+
 }
