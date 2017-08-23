dd90c40af18560da5b0c8a866a0fc133d0554a0b
Robson Cardozo <robson.cardozo.junior@gmail.com>
2017-08-23 20:24:35 -0300

    Criação de uma tela com tabela exibindo usuarios

--- src/main/java/com/br/pontu/controller/AdminController.java
+++ src/main/java/com/br/pontu/controller/AdminController.java
@@ -8,6 +8,7 @@ package com.br.pontu.controller;
 import com.br.pontu.entity.Adress;
 import com.br.pontu.entity.User;
 import com.br.pontu.services.UserServiceImpl;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -41,6 +42,17 @@ public class AdminController {
         userService.cadastrarUsuario(user);
         
         return ("redirect:/admin/registro");
+    }
+    
+    @GetMapping("/lista")
+    public ModelAndView listaUsers(User user){
+        ModelAndView model = new ModelAndView("/listaUsers");
+        List<User> list =  userService.getAllUsers();
+        
+        model.addObject("list",list);
+        
+        return model;
+        
     }
     
     
