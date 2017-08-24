43cdb246123771b1f0ad4b7a09b22675e7e74c69
Reinaldo Albernaz <reinaldoalbernaz@gmail.com>
2017-08-24 02:34:04 -0300

    Efeito visual na comparação de senhas

--- src/main/resources/static/dist/js/comparaSenha.js
+++ src/main/resources/static/dist/js/comparaSenha.js
@@ -6,14 +6,24 @@ function comparaSenha() {
     // testa para ver se os valores de senha e confirmação da senha estão em branco
     if (senha == "" || confirmaSenha == "") {
         if (senha == "") {
-            alert("Por favor preencha os campos de Senha!");
+            alert("Por favor preencha os campos de Senha!"); // alerta de campos vazios
         }
     } else {
-        var resultado = senha.localeCompare(confirmaSenha); // compara as senha e retorna -1 se forem diferentes
-        if(resultado == -1){
-           alert("Senhas não coincidem!"); 
+        var resultado = senha.localeCompare(confirmaSenha); // compara as senha e retorna -1 ou 1 se forem diferentes e 0 se forem iguais
+        if (resultado == 0) {
+            document.getElementById("senhaIncorreta").innerHTML = ""; // Reset do label
+            document.getElementById("senhaIncorreta").style.color = "#34495E"; //reset cor do campo 
+            document.getElementById('senha').className = 'form-group has-success'; // muda o estilo do formulário
+            document.getElementById('confirmaSenha').className ='form-group has-feedback has-success'; // muda o estilo do formulário
+            document.getElementById('iconConfirmaSenha').className = 'form-control-feedback fui-check'; // muda o simbolo de cross "X" para check "v"
+            
+        } else{
+            document.getElementById("senhaIncorreta").innerHTML = "As senhas não coincidem!"; // Mostra a mensagem de senha incorreta dentro da label
+            document.getElementById("senhaIncorreta").style.color = "#C0392B"; // muda a cor da fonte para vermelho POMEGRANATE
+            document.getElementById('confirmaSenha').className = 'form-group has-error'; // muda o estilo do formulário
+            document.getElementById('iconConfirmaSenha').className = 'form-control-feedback fui-cross'; // muda o simbolo de check "V" para cross "X"
+            
         }
-       // document.getElementById("demo").innerHTML = n;
     }
 
 }
