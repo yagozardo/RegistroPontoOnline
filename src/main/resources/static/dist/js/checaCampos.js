1236648cc7d6b91cf958f68964d620b13e3f4325
Reinaldo Albernaz <reinaldoalbernaz@gmail.com>
2017-09-04 22:45:30 -0300

    Auto stash before merge of "Develop" and "origin/Develop"

--- src/main/resources/static/dist/js/checaCampos.js
+++ src/main/resources/static/dist/js/checaCampos.js
@@ -1,213 +1,102 @@
 // JavaScript Document
+var val = 1; // variável para chegar se ocorreu alguma irregularidade
 
+/******************************** Módulos de Aviso **********************************/
+// função de aviso 
+function aviso(span, input) {
+    document.getElementById(span).style.color = "#F1C40F"; // muda a cor da fonte do aviso
+    document.getElementById(span).innerHTML = " Vazio!"; // muda o estilo do formulário
+    document.getElementById(input).className = 'form-group has-warning'; // muda o estilo do formulário
+    val++;
+}
+
+function erro(span, input) {
+    document.getElementById(span).style.color = "#E74C3C"; // muda a cor da fonte do aviso
+    document.getElementById(span).innerHTML = " Muito pequeno!"; // muda o estilo do formulário
+    document.getElementById(input).className = 'form-group has-error'; // muda o estilo do formulário
+    val++;
+}
+
+function sucesso(span, input) {
+    document.getElementById(span).style.color = "#2ECC71"; // muda a cor da fonte do aviso
+    document.getElementById(span).innerHTML = " OK!"; // muda o estilo do formulário
+    document.getElementById(input).className = 'form-group has-success'; // muda o estilo do formulário
+}
+/************************************************************************************/
+
+// Verifica a validade do nome
 function val_nome() {
-	var nome = document.forms.cadastro.fnome.value; // captura o valor do campo do nome
-	if (nome == "") {
-		cadastro.fnome.focus();
-		document.getElementById('input_nome').className = 'form-group has-warning'; // muda o estilo do formulário
-	} else {
-		document.getElementById('input_nome').className = 'form-group has-success'; // muda o estilo do formulário
-	}
+    var nome = document.forms.cadastro.fnome.value; // passa o valor do campo
+    if (nome === "") { // campo vazio
+        aviso("snome", "input_nome");
+        document.forms.cadastro.fnome.focus(); // foca no campo
+    } else if (nome.length <= 2) { // conteudo do campo insuficiente 
+        erro("snome", "input_nome");
+        document.forms.cadastro.fnome.focus();
+    } else {
+        sucesso("snome", "input_nome");
+    }
 }
 
+// Verifica a validade do sobrenome
 function val_sobrenome() {
-	var sobrenome = document.forms.cadastro.fsobreNome.value; // captura o valor do campo do sobrenome
-	if (sobrenome == "") {
-		cadastro.fsobreNome.focus();
-		document.getElementById('input_sobreNome').className = 'form-group has-warning'; // muda o estilo do formulário
-	} else {
-		document.getElementById('input_sobreNome').className = 'form-group has-success'; // muda o estilo do formulário
-	}
+    var sobrenome = document.forms.cadastro.fsobrenome.value; // passa o valor do campo
+    if (sobrenome === "") { // campo vazio
+        aviso("fsobrenome", "input_sobrenome");
+        document.forms.cadastro.fsobrenome.focus(); // foca no campo
+    } else if (sobrenome.length <= 2) { // conteudo do campo insuficiente 
+        erro("fsobrenome", "input_sobrenome");
+        document.forms.cadastro.fsobrenome.focus();
+    } else {
+        sucesso("fsobrenome", "input_sobrenome");
+    }
 }
-
-function val_data() {
-	document.forms.cadastro.fdataNasc.value; // captura o valor do campo de data
-	if (dataNascimento == "") {
-		cadastro.fdataNasc.focus();
-		document.getElementById('input_dataNascimento').className = 'form-group has-warning'; // muda o estilo do formulário
-		return false;
-	} else {
-		validaData();
-		document.getElementById('input_dataNascimento').className = 'form-group has-success'; // muda o estilo do formulário
-	}
-}
-
-function val_rg() {
-	var rg = document.forms.cadastro.frg.value; // captura o valor do campo de RG
-	if (rg == "") {
-		cadastro.frg.focus();
-		document.getElementById('input_rg').className = 'form-group has-warning'; // muda o estilo do formulário
-	} else {
-		document.getElementById('input_rg').className = 'form-group has-success'; // muda o estilo do formulário
-	}
-}
-
-function val_cpf() {
-	var cpf = document.forms.cadastro.fcpf.value; // captura o valor do campo de CPF
-	if (cpf == "") {
-		cadastro.fcpf.focus();
-		document.getElementById('input_cpf').className = 'form-group has-warning'; // muda o estilo do formulário
-	} else {
-		if(CPF(cpf)){
-			document.getElementById('input_cpf').className = 'form-group has-success'; // muda o estilo do formulário
-		} else {
-			document.getElementById('input_cpf').className = 'form-group has-error'; // muda o estilo do formulário
-			
-		}
-	}
-}
-
 
 function valida_form() {
 
-	var nome = document.forms.cadastro.fnome.value; // captura o valor do campo do nome
-	if (nome == "") {
-		bootbox.alert('<h6><b>Preencha o seu Nome!</b></h6>');
-		cadastro.fnome.focus();
-		document.getElementById('input_nome').className = 'form-group has-warning'; // muda o estilo do formulário
-		return false;
-	}
+    // Testa o valor do campo do nome
 
-	var sobrenome = document.forms.cadastro.fsobreNome.value; // captura o valor do campo do sobrenome
-	if (sobrenome == "") {
-		bootbox.alert('<h6><b>Preencha o seu Sobrenome!</b></h6>');
-		cadastro.fsobreNome.focus();
-		document.getElementById('input_sobreNome').className = 'form-group has-warning'; // muda o estilo do formulário
-		return false;
-	}
+    //window.setTimeout('val_nome()', 500); // chamada da função de verificar o nome
+    val_nome(); // chamada da função de verificar o nome
 
-	var dataNascimento = document.forms.cadastro.fdataNasc.value; // captura o valor do campo de data
-	if (dataNascimento == "") {
-		bootbox.alert('<h6><b>Preencha o sua Data Nascimento!</b></h6>');
-		cadastro.fdataNasc.focus();
-		document.getElementById('input_dataNascimento').className = 'form-group has-warning'; // muda o estilo do formulário
-		return false;
-	}
+    //val_sobrenome();
 
-	var rg = document.forms.cadastro.frg.value; // captura o valor do campo de RG
-	if (rg == "") {
-		bootbox.alert('<h6><b>Preencha o seu RG!</b></h6>');
-		cadastro.frg.focus();
-		document.getElementById('input_rg').className = 'form-group has-warning'; // muda o estilo do formulário
-		return false;
-	}
-
-	var cpf = document.forms.cadastro.fcpf.value; // captura o valor do campo de CPF
-	if (cpf == "") {
-		bootbox.alert('<h6><b>Preencha o seu CPF!</b></h6>');
-		cadastro.fcpf.focus();
-		document.getElementById('input_cpf').className = 'form-group has-warning'; // muda o estilo do formulário
-		return false;
-	}
-
-	var carteiraT = document.forms.cadastro.fct.value; // captura o valor do campo de Carteira de Trabalho
-	if (carteiraT == "") {
-		bootbox.alert('<h6><b>Preencha o sua Carteira de Trabalho!</b></h6>');
-		cadastro.fct.focus();
-		document.getElementById('input_carteiraDeTrabalho').className = 'form-group has-warning'; // muda o estilo do formulário
-		return false;
-	}
-
-	/*
-	    var estadoCivil = document.forms.cadastro.festadoCivil.value; // captura o valor do campo de Estado Civil
-	    if (estadoCivil == "") {
-	        bootbox.alert('<h6><b>Preencha o campo com seu Estado Civil!</b></h6>');
-	        cadastro.festadoCivil.focus();
-	        document.getElementById('input_estadoCivil').className = 'form-group form-control'; // muda o estilo do formulário
-	        return false;
-	    }
-
-	    var tipoAcesso = document.forms.cadastro.ftipoAcesso.value; // captura o valor do campo de Tipo de Acesso
-	    if (tipoAcesso == "") {
-	        bootbox.alert('<h6><b>Preencha o campo com seu Tipo de Acesso!</b></h6>');
-	        cadastro.ftipoAcesso.focus();
-	        document.getElementById('input_tipoAcesso').className = 'form-group form-control'; // muda o estilo do formulário
-	        return false;
-	    }
-
-	*/
-	var celular = document.forms.cadastro.fcelular.value; // captura o valor do campo do Celular
-	if (celular == "") {
-		bootbox.alert('<h6><b>Preencha o seu Número de Celular!</b></h6>');
-		cadastro.fcelular.focus();
-		document.getElementById('input_celular').className = 'form-group has-warning'; // muda o estilo do formulário
-		return false;
-	}
-
-	var cep = document.forms.cadastro.fcep.value; // captura o valor do campo do CEP
-	if (cep == "") {
-		bootbox.alert('<h6><b>Preencha o seu CEP!</b></h6>');
-		cadastro.fcep.focus();
-		document.getElementById('input_cep').className = 'form-group has-warning'; // muda o estilo do formulário
-		return false;
-	}
-
-	var logradouro = document.forms.cadastro.flogradouro.value; // captura o valor do campo do Logradouro
-	if (logradouro == "") {
-		bootbox.alert('<h6><b>Preencha o seu Logradouro!</b></h6>');
-		cadastro.flogradouro.focus();
-		document.getElementById('input_logradouro').className = 'form-group has-warning'; // muda o estilo do formulário
-		return false;
-	}
-
-	var complemento = document.forms.cadastro.fcomplemento.value; // captura o valor do campo do Complemento
-	if (logradouro == "") {
-		bootbox.alert('<h6><b>Preencha o seu Complemento!</b></h6>');
-		cadastro.fcomplemento.focus();
-		document.getElementById('input_complemeno').className = 'form-group has-warning'; // muda o estilo do formulário
-		return false;
-	}
-
-	var numero = document.forms.cadastro.fnumero.value; // captura o valor do campo do Número
-	if (numero == "") {
-		bootbox.alert('<h6><b>Preencha o seu Logradouro!</b></h6>');
-		cadastro.fcomplemento.focus();
-		document.getElementById('input_complemeno').className = 'form-group has-warning'; // muda o estilo do formulário
-		return false;
-	}
-
-	var cidade = document.forms.cadastro.fcidade.value; // captura o valor do campo do Cidade
-	if (cidade == "") {
-		bootbox.alert('Preencha o seu Cidade!</b></h6>');
-		cadastro.fcidade.focus();
-		document.getElementById('input_cidade').className = 'form-group has-warning'; // muda o estilo do formulário
-		return false;
-	}
-
-	/*var uf = document.forms.cadastro.fuf.value; // captura o valor do campo do Estado
-	if (uf == "") {
-	   bootbox.alert('<h6><b>Preencha o campo com seu Estado!</b></h6>');
-	    cadastro.fuf.focus();
-	    document.getElementById('input_uf').className = 'form-group form-control'; // muda o estilo do formulário
-	    return false;
-	}*/
-
-	var matricula = document.forms.cadastro.fmatricula.value; // captura o valor do campo do Matricula
-	if (matricula == "") {
-		bootbox.alert('<h6><b>Preencha o seu Matrícula!</b></h6>');
-		cadastro.fmatricula.focus();
-		document.getElementById('input_matricula').className = 'form-group has-warning'; // muda o estilo do formulário
-		return false;
-	}
-
-	var senha = document.forms.cadastro.fsenha.value; // captura o valor do campo de senha
-	if (senha == "") {
-		bootbox.alert('<h6><b>Preencha o sua Senha!</b></h6>');
-		cadastro.fsenha.focus();
-		document.getElementById('input_senha').className = 'form-group has-warning'; // muda o estilo do formulário
-		return false;
-	}
-
-	var confirmaSenha = document.forms.cadastro.fconfirmaSenha.value; // captura o valor do campo de confirmar a senha
-	if (confirmaSenha == "") {
-		bootbox.alert('<h6><b>Preencha o sua Confirmação da Senha!</b></h6>');
-		cadastro.fsenha.focus();
-		document.getElementById('input_senha').className = 'form-group has-warning'; // muda o estilo do formulário
-		return false;
-	}
+    // verifica se tem erros
+    if (val > 1) {
+        swal('Erros foram encontrados!');
+        return false;
+    } else {
+        return true;
+    }
 
 }
 
+
+/*function val_data() {
+    if (document.forms.cadastro.fdataNasc.value == "") {
+        document.forms.cadastro.fdataNasc.focus();
+        document.getElementById('input_dataNascimento').className = 'form-group has-warning'; // muda o estilo do formulário
+        return false;
+    } else {
+        var ano_nascimento = document.forms.cadastro.fdataNasc.value;
+        // captura apanas os 4 primeiros caracteres
+        ano_nascimento = ano_nascimento.slice(0, 4);
+        // pega o ano atual
+        var Data = new Date();
+        var ano_atual = Data.getFullYear();
+        // calcula a diferença para descobrir a idade
+        var idade = ano_atual - ano_nascimento;
+        if (idade >= 90 || idade <= 12) {
+            document.getElementById('input_dataNascimento').className = 'form-group has-error'; // muda o estilo do formulário
+            return false;
+        } else {
+            document.getElementById('input_dataNascimento').className = 'form-group has-success'; // muda o estilo do formulário
+            return true;
+        }
+    }
+}*/
+
+/*
 function comparaSenha() {
 
 	var senha = document.forms.cadastro.fsenha.value; // captura o valor do campo de senha
@@ -233,23 +122,4 @@ function comparaSenha() {
 			document.fconfirmaSenha.focus();
 		}
 	}
-}
-
-function validaData() {
-	var dataNascimento = document.forms.cadastro.fdataNasc.value; // captura o valor do campo de data
-	if (dataNascimento != "") { // virefica se a data nascimento tem menos de 10 anos ou mais de 100
-		var data = new Date();
-		var anoAtual = parseInt(data.getFullYear());
-		var dataNascimento = parseInt(dataNascimento).slice(0, 4);
-		var diferenca = anoAtual - dataNascimento;
-		alert(diferenca);
-		alert(diferenca);
-		if (diferenca >= 100 || diferenca <= 10) {
-			bootbox.alert('<h6><b>Data de Nascimento inválida!</b></h6>');
-		}
-	}
-}
-
-function valida_cpf(){
-	
-}
+}*/
