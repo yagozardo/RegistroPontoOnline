581c130192d61da820ab28f343dc4f483da7df03
Reinaldo Albernaz <reinaldoalbernaz@gmail.com>
2017-09-08 20:21:08 -0300

    checa celular e fixo

--- src/main/resources/static/dist/js/checaCampos.js
+++ src/main/resources/static/dist/js/checaCampos.js
@@ -209,7 +209,7 @@ function val_email() {
                 return false;
             }
         }
-        
+
         // Verifica o retorno da função da validação de emal
         if (!validarEmail(email)) {
             document.getElementById('semail').style.color = "#E74C3C"; // muda a cor da fonte do aviso
@@ -230,7 +230,7 @@ function val_fixo() {
     if (fixo === "") { // campo vazio
         aviso("sfixo", "input_fixo");
         document.forms.cadastro.ffixo.focus(); // foca no campo
-    } else if (fixo.length < 10) { // conteudo do campo insuficiente 
+    } else if (fixo.length < 14) { // conteudo do campo insuficiente 
         erro("sfixo", "input_fixo");
         document.forms.cadastro.ffixo.focus();
     } else {
@@ -443,6 +443,41 @@ function validarRG(numero) {
         return true;
     }
 }
+
+/**************** Máscaras *******************/
+// máscara para telefones
+function mask(e, id, mask) {
+    var tecla = (window.event) ? event.keyCode : e.which;
+    if ((tecla > 47 && tecla < 58)) {
+        mascara(id, mask);
+        return true;
+    } else {
+        if (tecla == 8 || tecla == 0) {
+            mascara(id, mask);
+            return true;
+        } else return false;
+    }
+}
+
+function mascara(id, mask) {
+    var i = id.value.length;
+    var carac = mask.substring(i, i + 1);
+    var prox_char = mask.substring(i + 1, i + 2);
+    if (i == 0 && carac != '#') {
+        insereCaracter(id, carac);
+        if (prox_char != '#') insereCaracter(id, prox_char);
+    } else if (carac != '#') {
+        insereCaracter(id, carac);
+        if (prox_char != '#') insereCaracter(id, prox_char);
+    }
+
+    function insereCaracter(id, char) {
+        id.value += char;
+    }
+}
+
+/**************** FIM Máscaras *******************/
+
 /*
 function comparaSenha() {
 
