8eb302d719d46f7bc822a33a667617811e7821ff
Unknown <marcello_doalves@hotmail.com>
2017-10-19 08:37:09 -0200

    Adicionando arquivos para validação no ServerSide

---
+++ src/main/java/com/br/pontu/exceptionhandler/PontuExceptionHandler.java
@@ -0,0 +1,17 @@
+package com.br.pontu.exceptionhandler;
+
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+/**
+ * Está classe será responsável por informar o usuário caso haja 
+ * algum tipo validação feita em 'Cliente-Side' não seja bem sucedida.
+ * 
+ * 
+ * 
+ * @author Alves
+ */
+public class PontuExceptionHandler extends ResponseEntityExceptionHandler{
+	
+	
+
+}
