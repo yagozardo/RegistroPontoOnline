3c3250a1412da1fe44a6652d2af79787ebbd5f4c
Unknown <marcello_doalves@hotmail.com>
2017-10-20 07:57:34 -0200

    Adicionando classe estática para construção das msg de erro

--- src/main/java/com/br/pontu/exceptionhandler/PontuExceptionHandler.java
+++ src/main/java/com/br/pontu/exceptionhandler/PontuExceptionHandler.java
@@ -7,8 +7,7 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
  * Está classe será responsável por informar o usuário caso haja 
  * algum tipo validação feita em 'Cliente-Side' não seja bem sucedida.
  * 
- * 
- * 
+ * @since 19/10/2017
  * @author Alves
  */
 
@@ -18,5 +17,50 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 public class PontuExceptionHandler extends ResponseEntityExceptionHandler{
 	
 	
+	
+	
+	
+	
+	
+	
+	
+	/**
+	 * Classe estática responsável apenas pela construção da mensagem de 
+	 * retorno para o usuário. Ela precisa de 2 paramêtros:
+	 * Mensagem para o usuário - mensagem clara. 
+	 * Mensagem para o desenvolvedor - erro de console.
+	 * 
+	 * @author Alves
+	 */
+	public static class Error {
+
+		// Attributes -------------------------
+		private String msgUser;
+		private String msgDevelop;
+
+		// Constuctor -------------------------
+		public Error(String msgUser, String msgDevelop) {
+			super();
+			this.msgUser = msgUser;
+			this.msgDevelop = msgDevelop;
+		}
+
+		// Getters and Setters ----------------
+		public String getMsgUser() {
+			return msgUser;
+		}
+
+		public void setMsgUser(String msgUser) {
+			this.msgUser = msgUser;
+		}
+
+		public String getMsgDevelop() {
+			return msgDevelop;
+		}
+
+		public void setMsgDevelop(String msgDevelop) {
+			this.msgDevelop = msgDevelop;
+		}
+	}
 
 }
