47de8fb15116f7acaae8bbd1bc3f877a87f435bf
Unknown <marcello_doalves@hotmail.com>
2017-10-31 16:40:16 -0200

    Função verificarDiaExistente para não permitir mais de um dia para o mesmo user implementada MAS ainda não testada.

--- src/main/java/com/br/pontu/services/PontoDataHoraServiceImpl.java
+++ src/main/java/com/br/pontu/services/PontoDataHoraServiceImpl.java
@@ -1,11 +1,10 @@
 package com.br.pontu.services;
 
+import static org.mockito.Mockito.RETURNS_DEEP_STUBS;
+
 import java.security.NoSuchAlgorithmException;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
-import java.time.LocalDate;
-import java.time.LocalTime;
-import java.time.format.DateTimeFormatter;
 import java.util.Calendar;
 import java.util.List;
 
@@ -37,71 +36,92 @@ public class PontoDataHoraServiceImpl implements PontoDataHoraService {
 	public boolean baterPonto(String matricula, String password) {
 
 		List<User> users = userService.findByMatricula(matricula);
-
 		User user = users.get(0);
 
 		if (verificarUserESenha(matricula, password, user)) {
 
+			// Pega a data e formata
 			DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
-		    Calendar dia = null;
-		    dia = Calendar.getInstance();
-		    String diaFormatado = dateFormat.format(dia.getTime());
+			Calendar dia = null;
+			dia = Calendar.getInstance();
+			String diaFormatado = dateFormat.format(dia.getTime());
 
-		    DateFormat hourFormat = new SimpleDateFormat("HH:mm");
-		    Calendar hour = null;
-		    hour = Calendar.getInstance();
-		    String horaFormatado = hourFormat.format(hour.getTime());
-	
+			// Pega as horas e formata
+			DateFormat hourFormat = new SimpleDateFormat("HH:mm");
+			Calendar hour = null;
+			hour = Calendar.getInstance();
+			String horaFormatado = hourFormat.format(hour.getTime());
 
-//			LocalDate data = LocalDate.now();
-//			DateTimeFormatter fomatterDate = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-//			data.format(fomatterDate);
-//
-//			LocalTime time = LocalTime.now();
-//			DateTimeFormatter formatterHour = DateTimeFormatter.ofPattern("HH:mm");
-//			time.format(formatterHour);
-
+			// Cria Instancias de PontoData e PontoHora
 			PontoData pdata = new PontoData();
 			PontoHora phora = new PontoHora();
 
 			
-			
-			
-			pdata.setDia(diaFormatado);
-			pdata.setUserId(user.getId());
-			
-			pdata = pontoDataRepository.save(pdata);
-			System.out.println("\n\n\nSalvou Data\n\n\n");
-			
-			phora.setHora(horaFormatado);
-			phora.setDataId(pdata.getId());
-
-			pontoHoraRepository.save(phora);
-			System.out.println("\\n\\n\\nSalvou Hora\n\n\n");
-			
-			return true;
-			
+			// Garante apenas uma data por usuário
+			Long dia_id = verificarDiaExistente(diaFormatado, user.getId());
+			if(dia_id != null) {
+				
+				// Salva a hora, e o ID do dia correspondente
+				phora.setHora(horaFormatado);
+				phora.setDataId(dia_id);
+				pontoHoraRepository.save(phora);
+				
+				return true;
+				
+			} else {
+				
+				// Salva o dia com a data, e o ID do usuário
+				pdata.setDia(diaFormatado);
+				pdata.setUserId(user.getId());
+				pdata = pontoDataRepository.save(pdata);
+				
+				// Salva a hora, e o ID do dia correspondente
+				phora.setHora(horaFormatado);
+				phora.setDataId(pdata.getId());
+				pontoHoraRepository.save(phora);
+				
+				return true;
+				
+			}
 		}
 
-		return true;
+		return false;
 	}
 
-	// Função que checa se o Usuário buscado do banco, tenha a matricula e a senha
-	// iguais as do banco.
+	// Função que verifica se já existe aquela data salva no banco
+	// caso exista = Retorna ID do dia
+	// caso contrário = Retorna null
+	private Long verificarDiaExistente(String diaFormatado, Long userId) {
+		
+		List<PontoData> datas = pontoDataRepository.findAll();
+		
+		for(int i = 0; i < datas.size(); i++) {
+			
+			if(diaFormatado.equals(datas.get(i).getDia())) {
+				
+				return datas.get(i).getId();
+			}
+		}
+		
+		return null;
+	}
+
+	// Função que checa se o Usuário buscado do banco, tenha a matricula e a senha iguais as do banco.
 	private Boolean verificarUserESenha(String matricula, String password, User user) {
 
 		try {
 
+			// Encripta se a senha recebida para comparar com a do banco
 			String passwordEncode = userService.encodePassword(password);
-			
-			System.out.println(passwordEncode);
 
 			// Checa se ambos tanto a matricula, quanto o password é o mesmo do banco
 			if (matricula.equals(user.getMatricula()) && passwordEncode.equals(user.getPassword())) {
 
 				return true;
 			}
-
+			
+			return false;
+			
 		} catch (NoSuchAlgorithmException e) {
 
 			e.printStackTrace();
