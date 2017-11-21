de24f6f684bad8e3a95cccdbe72db15657063289
Unknown <marcello_doalves@hotmail.com>
2017-11-21 14:56:13 -0200

    Aceitando mais sistemas operacionais.

--- src/main/java/com/br/pontu/config/dataSource.java
+++ src/main/java/com/br/pontu/config/dataSource.java
@@ -11,7 +11,9 @@ package com.br.pontu.config;
  */
 import java.io.FileInputStream;
 import java.util.Properties;
+
 import javax.sql.DataSource;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
@@ -23,34 +25,40 @@ import org.springframework.jdbc.datasource.DriverManagerDataSource;
 @Configuration
 public class dataSource {
 
-    @Bean
-    public DataSource dataSrc() {
+	@Bean
+	public DataSource dataSrc() {
 
-        
+		Properties props = new Properties();
+		FileInputStream arquivo = null;
+		String so = System.getProperty("os.name").toLowerCase();
 
-        Properties props = new Properties();
-        FileInputStream arquivo = null;
+		try {
+			
+			
+			if(so.equalsIgnoreCase("windows 10") || so.equalsIgnoreCase("windows 7")) {
+					
+				arquivo = new FileInputStream("C:\\Pontu\\datasource.properties");
+			}
+			if (so.equalsIgnoreCase("linux")) {
 
-        try {
+				arquivo = new FileInputStream("/usr/local/pontu/datasource.properties");
+			}
 
-            arquivo = new FileInputStream("C:\\Pontu\\datasource.properties");
-            props.load(arquivo);
+			props.load(arquivo);
 
+		} catch (Exception e) {
+			System.out.println("ERRO " + e);
+		}
 
-        } catch (Exception e) {
-            System.out.println("ERRO " + e);
-        }
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
 
-        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setDriverClassName(props.getProperty("spring.datasource.driver-class-name"));
+		dataSource.setUrl(props.getProperty("spring.datasource.url"));
+		dataSource.setUsername(props.getProperty("spring.datasource.username"));
+		dataSource.setPassword(props.getProperty("spring.datasource.password"));
 
-        dataSource.setDriverClassName(props.getProperty("spring.datasource.driver-class-name"));
-        dataSource.setUrl(props.getProperty("spring.datasource.url"));
-        dataSource.setUsername(props.getProperty("spring.datasource.username"));
-        dataSource.setPassword(props.getProperty("spring.datasource.password"));
+		return dataSource;
 
-        return dataSource;
-
-    }
+	}
 
 }
-
