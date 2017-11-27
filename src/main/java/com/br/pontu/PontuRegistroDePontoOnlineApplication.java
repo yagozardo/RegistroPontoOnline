8a9cc0cf1902def8206e19fbc0e3d7517adf061f
Marcello <marcello_doalves@hotmail.com>
2017-11-27 19:58:29 -0200

    Espelho de ponto arrumado

--- src/main/java/com/br/pontu/PontuRegistroDePontoOnlineApplication.java
+++ src/main/java/com/br/pontu/PontuRegistroDePontoOnlineApplication.java
@@ -2,9 +2,10 @@ package com.br.pontu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class PontuRegistroDePontoOnlineApplication {
+public class PontuRegistroDePontoOnlineApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(PontuRegistroDePontoOnlineApplication.class, args);
